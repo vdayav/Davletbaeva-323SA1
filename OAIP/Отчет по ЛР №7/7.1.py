3493535f19ed357ee97bc4a1264dbe4560ee1c78
Ilona <ilona_davletbaeva55@mail.ru>
2025-10-07 12:36:36 +0500

    Add files via upload

---
+++ OAIP/Отчет по ЛР №7/7.1.py
@@ -0,0 +1,12 @@
+dir = input("Куда идти? ")
+if dir == "left":
+    print("Иду влево")
+elif dir == "right":
+    print("Иду вправо")
+elif dir == "straight":
+    print("Иду прямо")
+elif dir == "back":
+    print("Иду назад")
+else: 
+    print("Неправильное направление")
+         
