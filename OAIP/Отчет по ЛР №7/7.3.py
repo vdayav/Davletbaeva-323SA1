0490ee149f27a33c8f50cb4929fd589c084f06e1
Ilona <ilona_davletbaeva55@mail.ru>
2025-10-07 13:18:22 +0500

    Delete OAIP/Отчет по ЛР №7/7.3.py

--- OAIP/Отчет по ЛР №7/7.3.py
+++
@@ -1,8 +0,0 @@
-text = input("Введите текст: ")
-word = input("Введите слово, которое хотите найти: ")
-if word in text:
-    count = text.count(word)
-    print("Слово", word, "найдено в тексте")
-    print(f"Сколько раз встречается в тексте: {count}")
-else:
-    print("Слова", word, "нет в тексте")
